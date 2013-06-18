a9018d301a2136b3e876ce10f4e099c0a3304598
Robert Beene <rbeene@teladoc.com>
2013-06-17 22:11:11 -0400

    Rails 4 setup
    * modifications to gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Studimetrics::Application.config.secret_key_base = '8bc55dc82eec0e00faf7e368888ab56614ef6c9e764aae39686a7ec10742e95a2e08a7b24429a16ef3137a811915a5e73e3f6e2eddb2f1cb80820b81a34797c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Studimetrics::Application.config.session_store :cookie_store, key: '_studimetrics_session'
