3c0972bfb7e2c193983beb670015dff6114ef97b
Robert Beene <robert@echobind.com>
2013-06-18 21:54:50 -0400

    Installation of clearance
    * Configure gemfile with Bcrypt
    * Add bin files
    * Add specs
    * Configure spec_helper to expose Capybara::DSL
    * Use env. variable for EMAIL_SERVER
    * Use env. variable for HOST (for URL's in emails)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,5 @@ Studimetrics::Application.configure do
   config.assets.debug = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.default_url_options = { :host => ENV['HOST']}
 end
