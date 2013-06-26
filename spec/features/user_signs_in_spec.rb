9fdc0048dae9c2a3ec485b0c116fa8c22694cddb
Chris Ball <chris@echobind.com>
2013-06-25 20:38:41 -0400

    add basic header and menu. direct to profile on sign in.

---
+++ spec/features/user_signs_in_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+feature 'user signs in' do
+  scenario 'starts on profile page' do
+    user = FactoryGirl.create :user
+
+    visit root_path
+    click_link 'sign in'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'password'
+
+    click_on 'Sign in'
+
+    expect(current_path).to eq(profile_path)
+  end
+
+  scenario 'should see name in header' do
+    user = FactoryGirl.create :user, first_name: 'The', last_name: 'Dude'
+
+    visit root_path as: user.id
+
+    expect(page).to have_content('The Dude')
+  end
+end
