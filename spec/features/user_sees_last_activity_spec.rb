f7df39157806484b278fb4796c6588b80bacfa90
Robert Beene <robert@echobind.com>
2013-07-05 08:37:02 -0400

    Last activity display
    * Add display of last activity
    * Add necssary css
    * Add helper for displaying the activity
    * Add necessary spec

---
+++ spec/features/user_sees_last_activity_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+feature 'user visits practice page' do
+
+  scenario 'sees list of tests' do
+    user_response = create :user_response
+    user = user_response.section_completion.user
+    visit root_path as: user.id
+
+    click_on 'Practice'
+
+    expect(page).to have_css "div#last-activity span.title", text: "Last Activity"
+    expect(page).to have_css "div#last-activity span.test", text: user_response.section_completion.section.practice_test_name
+    expect(page).to have_css "div#last-activity span.section", text: user_response.section_completion.section.name
+  end
+
+end
