4b664df27f8711db16620e00c3ba38c6de72bdbc
Robert Beene <robert@echobind.com>
2013-07-14 19:23:35 -0400

    Add robust seeds file
    * Create admins (include all 4 current) and a test user - non-admin
    * Create 2 books, 5 tests with 8 sections each with 20 questions per
    section
    * Dynamically create answers for each question
    
    schema update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,53 +6,91 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-admin = User.create(
-            email: "admin@example.com",
-            first_name: "Admin",
-            last_name: "User",
-            password: "test1234",
-            city: "Boston",
-            state: "MA",
-            grade: "12th",
-            admin: true)
+admins = [
+   {email: "chris@echobind.com", first_name: "Chris", last_name: "Ball"},
+   {email: "andrew.s.hannigan@gmail.com", first_name: "Andrew", last_name: "Hannigan"},
+   {email: "robert.beene@gmail.com", first_name: "Robert", last_name: "Beene"},
+   {email: "ryan.kim@studimetrics.com", first_name: "Ryan", last_name: "Kim"}
+]
+admins.each do |admin|
+  User.create(
+    email: admin[:email],
+    first_name: admin[:first_name],
+    last_name: admin[:last_name],
+    password: "test1234",
+    city: "Boston",
+    state: "MA",
+    grade: "12th",
+    admin: true)
+end
 
-user = User.create(
-            email: "user@example.com",
-            first_name: "Test",
-            last_name: "User",
-            password: "test1234",
-            city: "Boston",
-            state: "MA",
-            grade: "9th",
-            admin: false)
-
-book = Book.create(
+books = []
+books << Book.create(
             name: "Example Test Book",
-            publisher: Book::PUBLISHERS.first,
+            publisher: Book::PUBLISHERS.sample,
             publish_date: "2013-06-20")
 
-subject = Subject.create(name: "Math")
-test = PracticeTest.create(book: book, number: 1)
+books << Book.create(
+            name: "Example Test Book 2",
+            publisher: Book::PUBLISHERS.sample,
+            publish_date: "2013-02-24")
 
-section = Section.create(number: 1, practice_test: test, subject: subject)
 
-multiple_choice_question = Question.create!(
-  question_type: "Multiple Choice",
-  section: section,
-  position: 1)
+math_subject = Subject.create(name: "Math")
 
-MultipleChoiceAnswer.create!(
-  question: multiple_choice_question,
-  value: "A")
+concepts = []
+math_concept = {concepts: []}
+math_subject = Subject.create!(name: "Math")
 
-range_question = Question.create!(
-  question_type: "Range",
-  section: section,
-  position: 2)
+math_concept[:concepts] << Concept.create(subject: math_subject, name: "Algebra")
+math_concept[:concepts] << Concept.create(subject: math_subject, name: "Geometry")
+math_concept[:concepts] << Concept.create(subject: math_subject, name: "Angles on a Plane")
+math_concept[:concepts] << Concept.create(subject: math_subject, name: "Properties of Triangles")
 
-RangeAnswer.create!(
-  question: range_question,
-  min_value: 1,
-  max_value: 10)
+writing_subject = Subject.create!(name: "Writing")
 
+writing_concept = {concepts: []}
+writing_concept[:concepts] << Concept.create(subject: writing_subject, name: "Elements of Writing")
+writing_concept[:concepts] << Concept.create(subject: writing_subject, name: "Misplaced Modifiers")
+
+reading_subject = Subject.create!(name: "Critical Reading")
+reading_concept = {concepts: []}
+
+reading_concept[:concepts] << Concept.create(subject: reading_subject, name: "Comprehension")
+reading_concept[:concepts] << Concept.create(subject: reading_subject, name: "Analysis")
+reading_concept[:concepts] << Concept.create(subject: reading_subject, name: "Themes")
+
+concepts << math_concept
+concepts << writing_concept
+concepts << reading_concept
+
+question_types = ["Multiple Choice", "Free Response", "Range", "Multiple Choice", "Multiple Choice", "Multiple Choice"]
+
+books.each do |book|
+  5.times do |i|
+    practice_test = PracticeTest.create!(book: book, number: i+1)
+    8.times do |j|
+      concept = concepts.sample[:concepts].sample
+      subject = concept.subject rescue binding.pry
+      section = Section.create!(number: j+1, practice_test: practice_test, subject: subject)
+      20.times do |k|
+        question_type = question_types.sample
+        question_concepts = [subject.concepts.sample, subject.concepts.sample].uniq
+        question = Question.create!(question_type: question_types.sample, concept_ids: question_concepts.collect(&:id), section: section, position: k+1)
+
+        case question.answer_class.name
+          when "MultipleChoiceAnswer"
+            value = %w(A B C D E).sample
+            answer = question.answer_class.create!(question: question, value: value)
+          when "FreeResponseAnswer"
+            value = %w(1/5 2/3 5 26 43).sample
+            answer = question.answer_class.create!(question: question, value: value)
+          when "RangeAnswer"
+            min_value, max_value = [[1, 2.5], [2, 5], [4,4.35], [5, 25], [6, 10.5]].sample
+            answer = question.answer_class.create!(question: question, min_value: min_value, max_value: max_value)
+        end
+      end
+    end
+  end
+end
 
