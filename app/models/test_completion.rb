3b927efd58c1c7b6e56fe5db9fadebd516dd39e1
Robert Beene <robert@echobind.com>
2013-07-16 21:11:58 -0400

    Leverage scoreable boolean on section_completion
    * Update specs to use this
    * Update method retrieving section_completions

--- app/models/test_completion.rb
+++ app/models/test_completion.rb
@@ -8,7 +8,7 @@ class TestCompletion
   end
 
   def section_completions
-    @section_completions ||= SectionCompletion.select("DISTINCT ON (section_id, user_id) *").where(user: user, section_id: practice_test.section_ids).order("section_id, user_id, id asc")
+    @section_completions ||= SectionCompletion.where(user: user, section_id: practice_test.section_ids, scoreable: true).to_a
   end
 
   def retake_available?
