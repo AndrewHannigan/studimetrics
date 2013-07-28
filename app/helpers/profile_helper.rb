184cb85b167c0a2fd96e32a352588ce872c09a03
Robert Beene <robert@echobind.com>
2013-07-28 15:27:24 -0400

    Profile display of focus rank
    * Change profile show to display focus ranks - not concept progress
    * Add helper
    * Create partials for focus rank
    * Add spec

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,6 +1,14 @@
 module ProfileHelper
-  def concept_progress_table
-    concept_progresses = ConceptProgress.generate_for_user(current_user)
-    render "concept_progresses/table", concept_progresses: concept_progresses
+  def focus_ranks_table
+    focus_ranks = FocusRank.current_stats_for_user(current_user)
+    render "focus_ranks/table", focus_ranks: focus_ranks
+  end
+
+  def display_focus_rank_position(focus_rank)
+    render "focus_ranks/position", focus_rank: focus_rank
+  end
+
+  def display_focus_rank_accuracy(focus_rank)
+    render "focus_ranks/accuracy", focus_rank: focus_rank
   end
 end
