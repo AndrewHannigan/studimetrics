a82c64f6fa20ab1cd6bd35978b9617d22adde023
Robert Beene <robert@echobind.com>
2013-07-28 15:22:42 -0400

    Handled projected score when subject missing
    Return N/A - this becomes a problem in tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def projected_score_for_subject(subject)
-    return "N/A" unless current_user.is_a?(User)
+    return "N/A" if !current_user.is_a?(User) || !subject
     composite_score = CompositeScore.where(subject: subject, user: current_user).first
     composite_score.try(:projected_score) || "N/A"
   end
