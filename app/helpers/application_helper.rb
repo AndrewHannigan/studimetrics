ed321874de85c3b50a11d279db6c92e7a2bd9d87
Robert Beene <robert@echobind.com>
2013-07-28 09:45:06 -0400

    NullUser and projected score
    * If current user isn't a user, return N/A
    * I don't like this as Null object pattern means I'm now checking
    class.
    
    Conflicts:
    	app/helpers/application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 module ApplicationHelper
+  def projected_score_for_subject(subject)
+    return "N/A" unless current_user.is_a?(User)
+    composite_score = CompositeScore.where(subject: subject, user: current_user).first
+    composite_score.try(:projected_score) || "N/A"
+  end
 end
 
