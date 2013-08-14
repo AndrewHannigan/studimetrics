52e77ab93f017b53679e9ebee7ebfeee5e05ed27
Robert Beene <robert@echobind.com>
2013-08-14 09:24:38 -0400

    Alter composite score calculation
    * Calculate all concepts every time
    * Alter according to formula
    * Add question concept methods and specs

--- spec/models/composite_score_spec.rb
+++ spec/models/composite_score_spec.rb
@@ -43,97 +43,18 @@ describe CompositeScore do
   describe "#calculated_composite_score" do
     it "returns score according to formula" do
       subj = create :subject, name: "Math"
-      concepts = {"concept_1" => {correct: 2, incorrect: 3}, "concept_2" => {correct: 4, incorrect: 2}, "concept_3" => {correct: 5, incorrect: 0}}
-      composite_score = CompositeScore.new(subject: subj, concepts: concepts)
+      concept = create :concept, name: "Algrebra", subject: subj
+      concept2 = create :concept, name: "Geometry", subject: subj
+      user = create :user
+      composite_score = CompositeScore.new(subject: subj, user: user)
 
-      expect(composite_score.calculated_composite_score).to eq 37.046875
-    end
-  end
+      QuestionConcept.stubs(:percentage_frequency_for_concept).with(concept.id).returns(0.25)
+      QuestionConcept.stubs(:percentage_frequency_for_concept).with(concept2.id).returns(0.75)
+      composite_score.stubs(:accuracy_for_concept).with(concept.id).returns(0.50)
+      composite_score.stubs(:accuracy_for_concept).with(concept2.id).returns(0.20)
+      composite_score.stubs(:concept_ids_with_responses).returns([concept.id, concept2.id])
 
-  describe "#update_concept" do
-    context "2 correct answers for non free response questions" do
-      it "should have 2 correct, 0 incorrect" do
-        question = create :range_question, min_value: 2.0, max_value: 5
-        concept = create :concept, name: "Geometry"
-        create :question_concept, question: question, concept: concept
-        user_response = create :user_response, question: question, value: 2.5
-        user = user_response.section_completion.user
-
-        question2 = create :range_question, min_value: 2.0, max_value: 5
-        create :question_concept, question: question2, concept: concept
-        create :user_response, question: question2, value: 2.5, section_completion: user_response.section_completion
-
-        composite_score = CompositeScore.new(user: user, subject: concept.subject)
-
-        composite_score.update_concept(concept)
-
-        expect(composite_score.concepts["concept_#{concept.id}"][:correct]).to eq 2
-        expect(composite_score.concepts["concept_#{concept.id}"][:incorrect]).to eq 0
-      end
-    end
-
-    context "2 incorrect answers for non free response quesstions" do
-      it "should have 2 incorrect, 0 correct" do
-        question = create :range_question, min_value: 2.0, max_value: 5
-        concept = create :concept, name: "Geometry"
-        create :question_concept, question: question, concept: concept
-        user_response = create :user_response, question: question, value: 1.5
-        user = user_response.section_completion.user
-
-        question2 = create :range_question, min_value: 2.0, max_value: 5
-        create :question_concept, question: question2, concept: concept
-        create :user_response, question: question2, value: 1.5, section_completion: user_response.section_completion
-
-        composite_score = CompositeScore.new(user: user, subject: concept.subject)
-
-        composite_score.update_concept(concept)
-
-        expect(composite_score.concepts["concept_#{concept.id}"][:correct]).to eq 0
-        expect(composite_score.concepts["concept_#{concept.id}"][:incorrect]).to eq 2
-      end
-
-    end
-
-    context "1 correct answer for free response and 1 incorrect free response" do
-      it "should have 1 correct, 0 incorrect" do
-        question = create :free_response_question, value: 2.0
-        concept = create :concept, name: "Geometry"
-        create :question_concept, question: question, concept: concept
-        user_response = create :user_response, question: question, value: 2.0
-        user = user_response.section_completion.user
-
-        question2 = create :free_response_question, value: 2.0
-        create :question_concept, question: question2, concept: concept
-        create :user_response, question: question2, value: 1.5, section_completion: user_response.section_completion
-
-        composite_score = CompositeScore.new(user: user, subject: concept.subject)
-
-        composite_score.update_concept(concept)
-
-        expect(composite_score.concepts["concept_#{concept.id}"][:correct]).to eq 1
-        expect(composite_score.concepts["concept_#{concept.id}"][:incorrect]).to eq 0
-      end
-    end
-
-    context "2 incorrect answers for free response" do
-      it "should have 0 correct, 0 incorrect" do
-        question = create :free_response_question, value: 2.0
-        concept = create :concept, name: "Geometry"
-        create :question_concept, question: question, concept: concept
-        user_response = create :user_response, question: question, value: 1.5
-        user = user_response.section_completion.user
-
-        question2 = create :free_response_question, value: 2.0
-        create :question_concept, question: question2, concept: concept
-        create :user_response, question: question2, value: 1.5, section_completion: user_response.section_completion
-
-        composite_score = CompositeScore.new(user: user, subject: concept.subject)
-
-        composite_score.update_concept(concept)
-
-        expect(composite_score.concepts["concept_#{concept.id}"][:correct]).to eq 0
-        expect(composite_score.concepts["concept_#{concept.id}"][:incorrect]).to eq 0
-      end
+      expect(composite_score.calculated_composite_score).to eq 5.062500000000002
     end
   end
 
