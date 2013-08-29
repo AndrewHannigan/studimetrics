2dfd0b9f0c92a996eefca0f79d7df3eaefdf486c
Chris Ball <chris@echobind.com>
2013-08-28 22:20:34 -0400

    Filter concept sidebar
    Ryan wanted these changes: Let's get rid of every concept related to the
    reading section except for the vocabulary concept from the list on the
    left (navigation bar) from the Concept Videos page. We'll replace them
    with one new concept called "Reading Passage Questions."
    
    I'm doing this by getting all non-reading subjects combined with any
    hardcoded concepts in concept.rb

--- spec/models/concept_spec.rb
+++ spec/models/concept_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
 describe Concept do
+  describe '.for_sidebar' do
+    it 'gets a list of all concepts to display in the sidebar' do
+      reading_subject = create :subject, name: 'Reading'
+      concept1 = create :concept
+      concept2 = create :concept, subject: reading_subject
+      concept3 = create :concept, name: 'Vocabulary'
+
+      sidebar_concepts = Concept.for_sidebar
+      expect(sidebar_concepts).to include(concept1, concept3)
+      expect(sidebar_concepts).to_not include(concept2)
+    end
+  end
+
   describe '#underscored_concept_name' do
     it 'returns the underscored version of the concept name' do
       concept = Concept.new name: 'Angles on a plane'
