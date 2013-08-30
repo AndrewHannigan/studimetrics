fd72584bbc142ba4f58eab2f0f4ea0edb57c7e16
Robert Beene <robert@echobind.com>
2013-08-30 09:33:30 -0400

    Expire cache using Chris' suggestion

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -8,7 +8,9 @@ class Statistic
   def accuracy_for_subject_per_section_completion(subj)
     return [] unless subj
     subject_name = subj.name.titleize.gsub(/\//,' ').gsub(/\s+/, '_').underscore
-    Rails.cache.fetch("user_accuracy_per_section_completion_and_subject_#{subject_name}") do
+    section_completion = user.section_completions.last
+    return [] unless section_completion
+    Rails.cache.fetch("user_accuracy_per_section_completion_and_subject_#{subject_name}_#{section_completion.cache_key}") do
       user.section_completions.send(subject_name).map(&:accuracy)
     end
   end
