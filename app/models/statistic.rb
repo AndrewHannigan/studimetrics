885978a167af3572823a0ca505ad1baa747cc5c8
Robert Beene <robert@echobind.com>
2013-09-07 14:57:22 -0400

    Accuracy graph should only show completed sections
    * Add completed scope to section completion
    * Alter statistic accuracy method to use completed scope

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -11,7 +11,7 @@ class Statistic
     section_completion = user.section_completions.last
     return [] unless section_completion
     Rails.cache.fetch("user_accuracy_per_section_completion_and_subject_#{subject_name}_#{section_completion.cache_key}") do
-      user.section_completions.send(subject_name).map(&:accuracy)
+      user.section_completions.completed.send(subject_name).map(&:accuracy)
     end
   end
 
