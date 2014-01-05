f8f4e19aef865d070f2ccab6ae1fee535c6c1ab3
Andrew Hannigan <Andrew.S.Hannigan@Gmail.com>
2014-01-05 14:08:08 -0500

    added down method to migration

--- db/migrate/20131226190626_require_state_and_highschool.rb
+++ db/migrate/20131226190626_require_state_and_highschool.rb
@@ -1,8 +1,13 @@
 class RequireStateAndHighschool < ActiveRecord::Migration
   def up
-    User.update_all highschool: '', state: ''
-
+    User.update_all highschool: ''
+    User.where(state: nil).update_all state: ''
     change_column :users, :highschool, :string, null: false
     change_column :users, :state, :string, null: false 
+  end
+
+  def down
+    change_column :users, :highschool, :string
+    change_column :users, :state, :string 
   end
 end
