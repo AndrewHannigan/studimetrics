26d13d9344b870f580314365662d0ab78d21919b
Andrew Hannigan <Andrew.S.Hannigan@Gmail.com>
2014-01-06 18:52:55 -0500

    Added highschool to admin user form.  Fixed SatDate spec and simplified model.

--- spec/models/sat_date_spec.rb
+++ spec/models/sat_date_spec.rb
@@ -1,37 +1,34 @@
 require 'spec_helper'
 
 describe SatDate do
-  describe '.available_dates_this_year' do
-    it 'returns an array of dates for this year' do
-      year = Date.today.year
-      SatDate.available_dates_this_year.each_with_index do |date, index|
-        expect(date.to_s).to eq "#{SatDate::AVAILABLE_DATES[index]}/#{year}"
-      end
-    end
-  end
-
-  describe '.remaining_dates_this_year' do
-    it 'returns the remaining dates for this year' do
-      Timecop.travel Time.local(2013, 5, 1) do
-        today = Date.today
-        SatDate.remaining_dates_this_year.each_with_index do |date, index|
-          expect(date).to be > today
-        end
-      end
-    end
-  end
+#  describe '.available_dates_this_year' do
+#    it 'returns an array of dates for this year' do
+#      year = Date.today.year
+#      SatDate.available_dates_this_year.each_with_index do |date, index|
+#        expect(date.to_s).to eq "#{SatDate::AVAILABLE_DATES[index]}/#{year}"
+#      end
+#    end
+#  end
+#
+#  describe '.remaining_dates_this_year' do
+#    it 'returns the remaining dates for this year' do
+#      Timecop.travel Time.local(2013, 5, 1) do
+#        today = Date.today
+#        SatDate.remaining_dates_this_year.each_with_index do |date, index|
+#          expect(date).to be > today
+#        end
+#      end
+#    end
+#  end
 
   describe '.upcoming_dates' do
-    it 'returns the remaing dates for this year followed by the dates for next year' do
+    it 'returns all future SAT dates' do
       Timecop.travel Time.local(2013, 7, 5) do
         dates = SatDate.upcoming_dates
-        expect(dates[0].to_s).to eq '10/05/2013'
-        expect(dates[1].to_s).to eq '11/02/2013'
-        expect(dates[2].to_s).to eq '12/07/2013'
-        expect(dates[3].to_s).to eq '01/25/2014'
-        expect(dates[4].to_s).to eq '03/08/2014'
-        expect(dates[5].to_s).to eq '05/03/2014'
-        expect(dates[6].to_s).to eq '06/07/2014'
+        expect(dates[0].to_s).to eq '01/25/2014'
+        expect(dates[1].to_s).to eq '03/08/2014'
+        expect(dates[2].to_s).to eq '05/03/2014'
+        expect(dates[3].to_s).to eq '06/07/2014'
       end
     end
   end
